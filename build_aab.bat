c8f5b306c4fb9ad671f4db4ca694089959d136f1
Egg Targaryen <dzxrly2@gmail.com>
2024-01-15 15:18:03 +0800

    :art: 修改aab构建脚本以确保Play版与Github公开发行版版本号一致

--- build_aab.bat
+++ build_aab.bat
@@ -1,2 +1,2 @@
 echo "Building BF2042State for Android arm64..."
-flutter build appbundle --obfuscate --split-debug-info=./obfuscate  --dart-define=CHANNEL=play
+flutter build appbundle --obfuscate --split-debug-info=./obfuscate  --dart-define=CHANNEL=play --build-number=%1
