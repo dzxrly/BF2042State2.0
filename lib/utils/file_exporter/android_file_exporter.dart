9e526986fd6e2c6041721eb7420ae385be42baf8
Egg Targaryen <dzxrly2@gmail.com>
2024-02-11 11:49:56 +0800

    :bug: 修复了存储快照对Android API 30以下的系统出错的BUG

--- lib/utils/file_exporter/android_file_exporter.dart
+++ lib/utils/file_exporter/android_file_exporter.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:battlefield_2042_state/utils/tools.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:saf/saf.dart';
 
 FileExporter fileExporter() => AndroidFileExporter();
@@ -8,19 +11,43 @@ FileExporter fileExporter() => AndroidFileExporter();
 class AndroidFileExporter implements FileExporter {
   @override
   Future exportFile(String fileName, String content) async {
-    final safInstance = Saf('/storage/emulated/0/Documents');
-    bool? isGranted = await safInstance.getDirectoryPermission(isDynamic: true);
-    if (isGranted != null && isGranted) {
-      // Perform some file operations
-      final file = File('/storage/emulated/0/Documents/$fileName');
-      await file
-          .writeAsString(content)
-          .then((value) => {Future.value('success')})
-          .catchError((error) {
-        Future.error('error');
-      });
-    } else {
-      return Future.error('denied');
+    // in API 30 and above, dir is Documents, in API 29 and below, dir is Downloads
+    try {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      var dir = '/storage/emulated/0/Documents';
+      if (androidInfo.version.sdkInt <= 30) {
+        final Directory? downloadsDir = await getDownloadsDirectory();
+        if (downloadsDir != null) {
+          dir = downloadsDir.path.toString();
+        } else {
+          // create a Downloads directory in app directory
+          final Directory appDocDir = await getApplicationDocumentsDirectory();
+          dir = '${appDocDir.path}/Downloads';
+          final Directory newDir = Directory(dir);
+          if (!newDir.existsSync()) {
+            newDir.createSync();
+          }
+        }
+      }
+      log(dir);
+      final safInstance = Saf(dir);
+      bool? isGranted =
+          await safInstance.getDirectoryPermission(isDynamic: true);
+      if (isGranted != null && isGranted) {
+        // Perform some file operations
+        final file = File('$dir/$fileName');
+        await file
+            .writeAsString(content)
+            .then((value) => {Future.value('success')})
+            .catchError((error) {
+          throw 'error';
+        });
+      } else {
+        throw 'denied';
+      }
+    } catch (e) {
+      return Future.error('error');
     }
   }
 }
